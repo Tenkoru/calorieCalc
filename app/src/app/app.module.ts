d8811fb0f220c6d36812e1090fcb1c379905a274
Александр Стрелец <astrelets@inbox.ru>
2020-03-28 23:06:24 +0300

    add angularMaterial and form wrapper

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,15 +4,23 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {InputFormComponent} from "./input-form/input-form.component";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
